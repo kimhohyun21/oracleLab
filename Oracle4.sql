dec179524f41dd0e4e3e78f0fc729e3851688eec
Kim Ho Hyun <kimhohyun21@gmail.com>
2016-08-12 15:46:19 +0900

    Update Oracle4 file

--- Oracle4.sql
+++ Oracle4.sql
@@ -64,3 +64,37 @@
 -- <20> 입사일자가 81년도인 사람의 모든 정보를 출력하라
    SELECT * FROM emp WHERE hiredate LIKE '81%';
    SELECT * FROM emp WHERE TO_CHAR(hiredate, 'yy/mm/dd') LIKE '81%';
+
+-- <21> 이름 중 S자가 들어가 있는 사람만 모든 정보를 출력하라
+   SELECT * FROM emp WHERE ename LIKE '%S%';
+
+-- <22> 이름이 S로 시작하고 마지막 글자가 T인 사람의 모든 정보를 출력하라
+-- (단, 이름은 전체 5자리이다.)
+   SELECT  * FROM emp WHERE ename LIKE 'S%___T';
+
+-- <23> 첫 번째 문자는 관계없고 두 번째 문자가 A인  사람의 정보를 출력하라
+   SELECT * FROM emp WHERE ename LIKE '_A%';
+
+-- <24> 커미션이 NULL인 사람의 정보를 출력하라
+   SELECT * FROM emp WHERE comm IS NULL;
+
+-- <25> 커미션이 NULL이 아닌 사람의 모든 정보를 출력하라
+   SELECT * FROM emp WHERE comm IS NOT NULL;
+
+-- <26> 부서가 30번 부서이고 급여가, 500이상인 사람의 이름 부서, 월급을 출력하라
+   SELECT ename "이름", deptno "부서", sal "월급" FROM emp WHERE deptno=30 AND sal>=500;
+
+-- <27> 이름의 첫 글자가 K로 시작하거나 부서 번호가 30인 사람의 사원번호, 이름, 부서번호를 출력하라
+   SELECT empno 사원번호, ename 이름, deptno 부서번호 FROM emp WHERE ename LIKE 'K%' OR deptno=30;
+
+-- <28> 급여가 500이상이고 부서번호가 30번인 사원 중 직업이 MANAGER인 사람의 정보를 출력하라
+   SELECT * FROM emp WHERE sal>=500 AND deptno=30 AND job='MANAGER';
+
+-- <29> 부서 번호가 30인 사람 중 사원번호 (오름차순)으로 정렬하라
+   SELECT * FROM emp WHERE deptno=30 ORDER BY empno ASC;
+
+-- <30> 급여가 많은 순으로 정렬하라
+   SELECT * FROM emp ORDER BY sal DESC;
+
+-- <31> 부서번호로 ASCENDING SORT한 후 급여가 많은 사람 순으로 출력하라
+   SELECT * FROM emp ORDER BY deptno ASC, sal DESC;
