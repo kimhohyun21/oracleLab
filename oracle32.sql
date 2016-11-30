819c36b708a775bbbcbb1cce9f68364576ce6bc1
Kim Ho Hyun <kimhohyun21@gmail.com>
2016-11-30 14:11:48 +0900

    Initial commit (cherry picked from commit 40c3b37)

--- oracle32.sql
+++ oracle32.sql
@@ -10,11 +10,9 @@
   스토리
   순위
   별점
-  등급
 
 */
 
-DROP TABLE bigMovie CASCADE CONSTRAINTS;
 CREATE TABLE bigMovie(
   mno              NUMBER,
   title               VARCHAR2(260) CONSTRAINT bm_title_nn NOT NULL,
@@ -24,7 +22,7 @@ CREATE TABLE bigMovie(
   poster           VARCHAR2(1000),
   story             CLOB,
   rank              VARCHAR2(20),
-  star               VARCHAR2(20),
-  grade            VARCHAR2(20),
+  star              VARCHAR2(20),
   CONSTRAINT bm_mno_pk   PRIMARY KEY(mno)
 );
+/
